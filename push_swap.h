88a371a53f6f7ffcea40efb2964a69ed350d7509
thatimoreira <thatimoreira1@gmail.com>
2024-03-11 21:07:47 -0300

    Added 42 header, header rules and added libs used untill now

---
+++ push_swap.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsoares- <tsoares-@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/11 18:23:00 by tsoares-          #+#    #+#             */
+/*   Updated: 2024/03/11 18:23:52 by tsoares-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <unistd.h>
+# include "includes/Libft/libft.h"
+# include "includes/Printf/libftprintf.h"
+
+#endif
