15b663358c544791b360dca21b85e7011b406d65
thatimoreira <thatimoreira1@gmail.com>
2024-03-11 21:11:56 -0300

    Added Error message when no params are passed or if the param is an empty string and started to implement the logic to separate the params to later analize if they are all valid

--- push_swap.c
+++ push_swap.c
@@ -1,8 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsoares- <tsoares-@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/11 19:20:07 by tsoares-          #+#    #+#             */
+/*   Updated: 2024/03/11 19:20:11 by tsoares-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
-// create function read_input ()
+
+//void int  validate_number()
+
+/*static void  extract_numbers(char *argv_params, int start, int len)
+{
+    char *substr;
+
+    substr = "";
+    while (index >= 0)
+    {
+        while (argv_params)
+            
+        ft_printf("argv[1][%d]: %c\n", index, argv_params[index]);
+        //na extract_numbers call validate_input(argv[1][i++]);
+        index--;
+    }
+    validate_input(substr);
+}*/
+
 // that splits the argument and converts (use atoi) each nbr into integers
 
-// validate_input()
+// void validate_input(char *nbr)
 // checks if args are all int, bigger than INT_MAX or smaller than INT_MIN
 // checks if args are duplicated
 // in the above cases display "Error"
@@ -13,9 +43,41 @@
 // store each nbr in stack a
 int main(int argc, char **argv)
 {
+    int index;
+    char    **str;
+    //int     count;
+
+    index = 0;
+    str = NULL;
+    //count = 0;
     if (argc == 1 || (argc == 2 && !argv[1][0]))
+    {
+        ft_printf("Error");
         return (-1);
-    else if (argc == 2)
-        read_input(argv[1]); //add params
+    }
+    else if (argc == 2) // params passed between ""
+    {
+        str = ft_split(argv[1], ' ');
+        index = 0;
+        while (str[index])
+        {
+            ft_printf("str[%d]: %s\n", index, str[index]);
+            free(str[index]);
+            index++;
+        }
+        free(str);
+            //validate_input(str[index--]);
+        
+    }
+    else if (argc > 2) // params passed without ""
+    {
+        index = --argc;
+        while (index >= 1)
+        {
+            ft_printf("argv[%d]: %s\n", index, argv[index]);
+            //validate_input(argv[index--]);
+            index--;
+        }
+    }
     return (0);
 }
