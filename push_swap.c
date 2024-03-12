510c715082248e73080aaf2f8bdd7d66b300a55f
thatimoreira <thatimoreira1@gmail.com>
2024-03-12 15:51:15 -0300

    Solved memory leak when received params between quotation marks in the command line

--- push_swap.c
+++ push_swap.c
@@ -11,45 +11,33 @@
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-//void int  validate_number()
-
-/*static void  extract_numbers(char *argv_params, int start, int len)
+/*
+static int  extract_numbers(char *argv_param) //each argv_param is a substr
 {
-    char *substr;
+    size_t  i;
+    int     nbr;
+    static int  count; // counts the amount of numbers
 
-    substr = "";
-    while (index >= 0)
-    {
-        while (argv_params)
-            
-        ft_printf("argv[1][%d]: %c\n", index, argv_params[index]);
-        //na extract_numbers call validate_input(argv[1][i++]);
-        index--;
-    }
-    validate_input(substr);
+    i = 0;
+    nbr = 0;
+    count = 0;
+    //ft_atoi();
+    //validate_input();
+    count++;
+    return (nbr);
 }*/
-
-// that splits the argument and converts (use atoi) each nbr into integers
-
-// void validate_input(char *nbr)
-// checks if args are all int, bigger than INT_MAX or smaller than INT_MIN
-// checks if args are duplicated
-// in the above cases display "Error"
-// if not, initialize the stacks a and b
 
 // stack_init()
 // init stacks a and b
 // store each nbr in stack a
+
 int main(int argc, char **argv)
 {
     int index;
     char    **str;
-    //int     count;
 
     index = 0;
     str = NULL;
-    //count = 0;
     if (argc == 1 || (argc == 2 && !argv[1][0]))
     {
         ft_printf("Error");
@@ -58,26 +46,20 @@ int main(int argc, char **argv)
     else if (argc == 2) // params passed between ""
     {
         str = ft_split(argv[1], ' ');
-        index = 0;
         while (str[index])
         {
             ft_printf("str[%d]: %s\n", index, str[index]);
-            free(str[index]);
             index++;
         }
-        free(str);
-            //validate_input(str[index--]);
-        
+            //validate_input(str[index++]);
+        free_memory(str);
     }
-    else if (argc > 2) // params passed without ""
+    else // if (argc > 2) => params passed without ""
     {
-        index = --argc;
-        while (index >= 1)
-        {
+        while (argv[++index])
             ft_printf("argv[%d]: %s\n", index, argv[index]);
-            //validate_input(argv[index--]);
-            index--;
-        }
+            //extract_numbers(argv[index]);
     }
+        // stack init aqui passando str de nbrs e total nbrs
     return (0);
 }
