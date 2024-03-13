1b71b5307e897006407228b56ef9a4e4b2da88b5
thatimoreira <thatimoreira.dev@gmail.com>
2024-03-13 18:59:34 -0300

    Moved structure that creates type node to stack.h file inside stack folder

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: tsoares- <tsoares-@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 18:23:00 by tsoares-          #+#    #+#             */
-/*   Updated: 2024/03/11 18:23:52 by tsoares-         ###   ########.fr       */
+/*   Updated: 2024/03/13 16:33:49 by tsoares-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,6 @@
 # include "includes/Printf/libftprintf.h"
 
 # define PRINT_ERROR ft_printf("Error\n");
-
-typedef struct  stack
-{
-    int num;
-    struct stack *next;
-} stack;
 
 void    free_memory(char **program_params);
 
