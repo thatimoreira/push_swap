f6e42f2a207a1d66a8cb9bbd9cd37089a8e02824
thatimoreira <thatimoreira.dev@gmail.com>
2024-03-13 18:54:22 -0300

    Adapted ft_atoi to push_swap project and now it doesn't ignore spaces or tabs

--- includes/Libft/ft_atoi.c
+++ includes/Libft/ft_atoi.c
@@ -6,44 +6,15 @@
 /*   By: tsoares- <tsoares-@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 16:57:11 by tsoares-          #+#    #+#             */
-/*   Updated: 2023/11/25 19:18:41 by tsoares-         ###   ########.fr       */
+/*   Updated: 2024/03/13 18:52:38 by tsoares-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../push_swap.h"
-
-static int	check_intmin_intmax(int nbr)
-{
-	if (nbr <= INT_MIN || nbr >= INT_MAX)
-	{
-		ft_printf("Error\n"); // substitute to a PRINTERROR
-		return (-2);
-	}
-	return (nbr);
-}
-
-static int	validate_nbr(char *str)
-{
-	int	pos;
-
-	pos = 0;
-	while (str[pos])
-	{
-		if (str[pos] >= '0' && str[pos] <= '9')
-			pos++;
-		else
-		{
-			ft_printf("Error\n"); // substitute to a PRINTERROR
-			return (-2);
-		}
-	}
-	return (1);
-}
+#include <stdio.h>
 
 int	ft_atoi(const char *nptr)
 {
-// args are bigger than INT_MAX or smaller than INT_MIN?
-// in the above cases display "Error"
 	int		i;
 	int		convnum;
 	int		sign;
@@ -51,27 +22,20 @@ int	ft_atoi(const char *nptr)
 	i = 0;
 	convnum = 0;
 	sign = 1;
-	//while ((nptr[i] >= '\t' && nptr[i] <= '\r') || nptr[i] == ' ')
-	//	i++;
-	if (nptr[i] == '-' || nptr[i] == '+')  // mantain the positive signal???
+
+	if (nptr[i] == '-')
 	{
-		if (nptr[i++] == '-')
-			sign = -1;
-		if (!nptr[i])
-		{
-			ft_printf("Error\n"); // substitute to a PRINTERROR
-			return (-2);
-		}
+		sign = -1;
+		i++;
 	}
-	if (validate_nbr((char *)(nptr + i)))
+	if (!nptr[i])
 	{
-		while (nptr[i] >= '0' && nptr[i] <= '9')
-			convnum = convnum * 10 + (nptr[i++] - 48);
-		convnum = check_intmin_intmax(convnum * sign);
-		return (convnum);
-	}
-	else
+		printf("Error\n"); // substitute to a PRINTERROR
 		return (-2);
+	}
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+		convnum = convnum * 10 + (nptr[i++] - 48);
+	return (convnum * sign);
 }
 
 int	main(void)
@@ -79,8 +43,8 @@ int	main(void)
 	char	*str;
 	int		convertstr;
 
-	str = "2147483646";
+	str = "2147483648";
 	convertstr = ft_atoi(str);
-	ft_printf("%d\n", convertstr);
+	printf("%d\n", convertstr);
 	return (0);
 }
